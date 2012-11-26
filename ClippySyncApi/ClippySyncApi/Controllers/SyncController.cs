fb9ac9696b8aed3a019ac8e3c0fbf32bda24f26a
Al Pascual <alpascual@gmail.com>
2012-11-25 16:35:09 -0800

    all cahnges done

---
+++ ClippySyncApi/ClippySyncApi/Controllers/SyncController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClippySyncApi.Controllers
+{
+    public class SyncController : Controller
+    {
+        //
+        // GET: /Sync/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public int SendClipboard(string UserToken, string Clipboard)
+        {
+
+            return 0;
+        }
+
+        public string GetClipboard(string UserToken, int SequenceNumber)
+        {
+            return "";
+        }
+
+    }
+}
---
+++ ClippySyncApi/ClippySyncApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClippySyncApi.MvcApplication" Language="C#" %>
