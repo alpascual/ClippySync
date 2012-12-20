c00080d470816b0ba19d3d6f162029804b6c775b
Al <alpascual@gmail.com>
2012-12-19 20:39:18 -0800

    project settings fixed again

---
+++ ClippyMacClient/ClippyMacClient/NSString+Addition.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Addition.h
+//  ClippyMacClient
+//
+//  Created by Albert Pascual on 12/19/12.
+//  Copyright (c) 2012 Esri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface NSString (NSStringAdditions)
+//
+//+ (NSString *) base64StringFromData:(NSData *)data length:(int)length;
+//
+//@end
