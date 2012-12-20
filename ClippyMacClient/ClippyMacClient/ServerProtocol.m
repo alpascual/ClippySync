f7aa35a2336c92f26e4362aab18dbfe43515ae4d
Al <alpascual@gmail.com>
2012-12-19 20:47:09 -0800

    save C#

--- ClippyMacClient/ClippyMacClient/ServerProtocol.m
+++ ClippyMacClient/ClippyMacClient/ServerProtocol.m
@@ -25,4 +25,71 @@
     
     return get;
 }
+
+/*
+ public bool ServerRegistration(string Username, string Password)
+ {
+ string sResponse = RequestToServer("Register?username=" + Encrypter.base64Encode(Username) + "&Password=" + Encrypter.base64Encode(Password) + "&Version=1");
+ 
+ if (sResponse == "")
+ return true;
+ 
+ return false;
+ }
+ public bool ServerLogin(string Username, string Password)
+ {
+ string sResponse = RequestToServer("Login?username=" + Encrypter.base64Encode(Username) + "&Password=" + Encrypter.base64Encode(Password) + "&Version=1");
+ 
+ return Convert.ToBoolean(sResponse);
+ }
+ 
+ public double SendTextToClipboard(string Username, string Password, string sClipboard)
+ {
+ string sRequestString = "SendClipboard?username=" + Encrypter.base64Encode(Username) + "&Password=" + Encrypter.base64Encode(Password) + "&Clipboard=" + Encrypter.base64Encode(sClipboard) + "&Version=1";
+ string sResponse = RequestToServer(sRequestString);
+ 
+ double SyncID = 0;
+ try
+ {
+ Console.WriteLine("DEBUG: response: " + sResponse);
+ SyncID = Convert.ToDouble(sResponse);
+ }
+ catch
+ {
+ SyncID = -1;
+ }
+ 
+ return SyncID;
+ }
+ 
+ public string GetTextFromClipboard(string Username, string Password, double SequenceNumber)
+ {
+ string sRequestString = "GetClipboard?username=" + Encrypter.base64Encode(Username) + "&Password=" + Encrypter.base64Encode(Password) +
+ "&SequenceNumber=" + SequenceNumber + "&version=1";
+ 
+ // Timeouts happen
+ string sResponse = "";
+ try
+ {
+ sResponse = RequestToServer(sRequestString);
+ }
+ catch { }
+ 
+ 
+ return sResponse;
+ }
+ 
+ public string RegistrationUrl()
+ {
+ return Server.Url.Replace("sync/", "Account/Register");
+ }
+ 
+ private string RequestToServer(string sUrlRequest)
+ {
+ string sUrl = Server.Url + sUrlRequest;
+ 
+ WebClient client = new WebClient();
+ return client.DownloadString(sUrl);
+ }*/
+
 @end
