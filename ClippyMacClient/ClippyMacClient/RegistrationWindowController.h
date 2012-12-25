a6e00e5d91aca0191f463efe05d961511a0c76c7
Al <alpascual@gmail.com>
2012-12-24 18:18:38 -0800

    save again

--- ClippyMacClient/ClippyMacClient/RegistrationWindowController.h
+++ ClippyMacClient/ClippyMacClient/RegistrationWindowController.h
@@ -1,22 +1,18 @@
 //
-//  LoginWindowController.h
+//  RegistrationWindowController.h
 //  ClippyMacClient
 //
-//  Created by Al Pascual on 12/12/12.
+//  Created by Albert Pascual on 12/24/12.
 //  Copyright (c) 2012 Esri. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-#import "ServerProtocol.h"
-
-@interface LoginWindowController : NSWindowController
+@interface RegistrationWindowController : NSWindowController
 
 @property (nonatomic, strong) IBOutlet NSTextField *textEmail;
 @property (nonatomic, strong) IBOutlet NSTextField *textPassword;
-@property (nonatomic, strong) IBOutlet NSButton *buttonLogin;
+@property (nonatomic, strong) IBOutlet NSTextField *textRepeatPassword;
 @property (nonatomic, strong) IBOutlet NSButton *buttonRegister;
-
-@property (nonatomic, strong) ServerProtocol *serverProtocol;
 
 @end
---
+++ ClippyMacClient/ClippyMacClient/RegistrationWindowController.m
@@ -0,0 +1,34 @@
+//
+//  RegistrationWindowController.m
+//  ClippyMacClient
+//
+//  Created by Albert Pascual on 12/24/12.
+//  Copyright (c) 2012 Esri. All rights reserved.
+//
+
+#import "RegistrationWindowController.h"
+
+@interface RegistrationWindowController ()
+
+@end
+
+@implementation RegistrationWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
